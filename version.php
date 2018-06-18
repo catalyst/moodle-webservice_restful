29bff2ea207b14cd210d62d76ea6e87de6da0b97
Matt Porritt <mattp@catalyst-au.net>
2018-06-18 15:00:14 +1000

    version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018052300;            // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2018052300;            // Same as version.
+$plugin->version   = 2018061800;            // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2018061800;            // Same as version.
 $plugin->component = 'webservice_restful';  // Full name of the plugin (used for diagnostics).
 $plugin->requires = 2016052300;             // Requires this Moodle version.
 $plugin->maturity = MATURITY_STABLE;
